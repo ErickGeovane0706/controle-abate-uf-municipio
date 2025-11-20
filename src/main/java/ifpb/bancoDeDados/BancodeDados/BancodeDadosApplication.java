f495dcce4bae1732cc25239da1cc1dd68a2eded1
Erick <erickgeovane2002@gmail.com>
2025-11-20 16:58:02 -0300

    Criação do Projeto

---
+++ src/main/java/ifpb/bancoDeDados/BancodeDados/BancodeDadosApplication.java
@@ -0,0 +1,13 @@
+package ifpb.bancoDeDados.BancodeDados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodeDadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifpb/bancoDeDados/BancodeDados/BancodeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpb.bancoDeDados.BancodeDados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodeDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
